b9c9295d99cd2577d62fd6e35a5123a22858a5f5
FosterGun <fostergun123@gmail.com>
2017-04-26 23:31:27 +0200

    LMAO

--- src/main/java/discordbot/App.java
+++ src/main/java/discordbot/App.java
@@ -7,7 +7,7 @@ package discordbot;
 public class App {
 	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //Hi, i'm a hackerman!!!
     }
     
 }
