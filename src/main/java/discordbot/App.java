bccba6e6d7bdf68854cb3149d3d8461589ba0d0d
FosterGun <fostergun123@gmail.com>
2017-04-27 21:58:42 +0200

    I'm in good way!!

--- src/main/java/discordbot/App.java
+++ src/main/java/discordbot/App.java
@@ -1,5 +1,11 @@
 package discordbot;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+
 /**
  * Hello world!
  *
@@ -7,7 +13,18 @@ package discordbot;
 public class App {
 	
     public static void main(String[] args) {
-        System.out.println("Hello World!"); //Hi, i'm a hackerman!!!
+        try {
+        	String config = new String(Files.readAllBytes(Paths.get("config/bot.json")));
+        	JSONObject configJson = new JSONObject(config);
+        	String botToken = configJson.getString("botToken");
+        	String game = configJson.getString("game");
+        	String version = configJson.getString("version");
+        	String clientID = configJson.getString("clientID");
+        	
+        	
+        } catch(IOException e) {
+        	System.out.println("Bot Config not found!");
+        }
     }
     
 }
